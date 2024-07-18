0f1e8059a260fbc448d76b4099136ee752769d97
1Haun <huanbv611@hotmail.com>
2024-07-18 14:40:40 +0700

    refactor theme color

--- src/pages/Boards/BoardContent/index.jsx
+++ src/pages/Boards/BoardContent/index.jsx
@@ -3,13 +3,15 @@ import Box from '@mui/material/Box'
 function BoardContent() {
   return (
     <Box sx={{
-      backgroundColor: 'primary.main',
+      bgcolor: (theme) => (
+        theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
+      ),
       with: '100%',
       height: (theme) => `calc(100vh - ${theme.trello.appBarHeight} - ${theme.trello.broadBarHeight})`,
       display: 'flex',
       alignItems: 'center'
     }}>
-        Broad Content
+      Broad Content
     </Box>
   )
 }
