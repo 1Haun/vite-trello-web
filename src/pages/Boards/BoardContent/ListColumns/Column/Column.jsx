76eb2a9f31bdf7db706a80b25b7ca569e2391698
1Haun <huanbv611@hotmail.com>
2024-07-20 15:23:04 +0700

    mock sort data

--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -17,8 +17,9 @@ import AddCardIcon from '@mui/icons-material/AddCard'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import Box from '@mui/material/Box'
 import ListCards from './ListCards/ListCards'
+import { mapOrder } from '~/utils/sorts'
 
-function Column() {
+function Column({ column }) {
   const [anchorEl, setAnchorEl] = useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => {
@@ -27,6 +28,8 @@ function Column() {
   const handleClose = () => {
     setAnchorEl(null)
   }
+
+  const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
   return (
     <Box sx={{
       minWidth: '300px',
@@ -51,7 +54,7 @@ function Column() {
           fontWeight: 'bold',
           cursor: 'pointer'
         }}>
-                    Column Title
+          {column?.title}
         </Typography>
         <Box>
           <Tooltip title="More options">
@@ -115,7 +118,7 @@ function Column() {
       </Box>
 
       {/* list card */}
-      <ListCards />
+      <ListCards cards={orderedCards} />
 
       {/* Box column footer */}
       <Box sx={{
